98a0ad85884b6e8dc5e92bf254a7242532d5a1bd
Sanchit-singhs <sanchitsingh849@gmail.com>
2023-03-04 20:16:34 +0530

    Modified templates to display posts from database.

--- inventory/views.py
+++ inventory/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import User_s
 
 # Create your views here.
 def home(request):
-    return render(request, 'inventory/home.html', {})
+    inventories = User_s.objects.all()
+    return render(request, 'inventory/home.html', {'inventories':inventories})
