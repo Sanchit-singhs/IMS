441a621cb5af580eed42e5f82b79c4c21bb58635
Sanchit-singhs <sanchitsingh849@gmail.com>
2023-03-04 22:40:22 +0530

    Made changes

--- inventory/urls.py
+++ inventory/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('User_s/<int:pk>/', views.user_detail, name='user_detail'),
 ]
--- inventory/views.py
+++ inventory/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import User_s
 
 # Create your views here.
 def home(request):
     inventories = User_s.objects.all()
     return render(request, 'inventory/home.html', {'inventories':inventories})
+
+def user_detail(request, pk):
+    inventory = get_object_or_404(User_s, pk=pk)
+    return render(request, 'inventory/user_detail.html', {'post': inventory})
